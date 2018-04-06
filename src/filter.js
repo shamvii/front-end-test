dbbe2fcc5f6d1a2a490e6fae07a89bad5abbe754
Shambhavi Shukla <shambhavishukla24@gmail.com>
2018-04-05 22:34:31 -0400

    initial commit

---
+++ src/filter.js
@@ -0,0 +1,11 @@
+{/* <Button onClick={reverseData => this.setState({clicked: false})} /> 
+class Button extends Component {
+    render() {
+      return (
+        <div>
+          <button type="submit" onClick={event => this.props.onClick(event.target.value) }>Sort</button>
+        </div>
+  
+      )
+    }
+  } */}
